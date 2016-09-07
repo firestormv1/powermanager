ed70e98eec16395d9913f77900035d7318fe94c7
firestormv1 <firestorm.v1@gmail.com>
2016-09-07 22:38:11 +0000

    test

--- octoprint_powermanager/__init__.py
+++ octoprint_powermanager/__init__.py
@@ -3,10 +3,23 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin):
+class PowerManagerPlugin(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("Hello World!")
+        self._logger.info("Hello World! more %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(selt):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+    def get_template_vars(self):
+        return dict(url=self._settings.get(["url"]))
+
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]
 
 __plugin_name__ = "PowerManager"
-__plugin_implementation__ = HelloWorldPlugin()
+__plugin_implementation__ = PowerManagerPlugin()
