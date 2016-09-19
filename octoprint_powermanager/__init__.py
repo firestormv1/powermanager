e3ced138e071ab732e6ef48f53ff1236fa34f3fd
firestormv1 <firestorm.v1@gmail.com>
2016-09-19 21:51:18 +0000

    Updates, dropped fan control since it wasn't possible via hardware

--- octoprint_powermanager/__init__.py
+++ octoprint_powermanager/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 import octoprint.plugin
 import octoprint.settings
 import RPi.GPIO as GPIO
-
+from subprocess import call
 
 
 
@@ -16,30 +16,63 @@ class PowerManagerPlugin(octoprint.plugin.StartupPlugin,
     def initialize(self):
 	#Load config vars
         self.powergpiopin = self._settings.get(["powergpiopin"])
-        self.fangpiopin = self._settings.get(["fangpiopin"])
         self.printergpiopin = self._settings.get(["printergpiopin"])
+	self.lightgpiopin self._settings.get(["lightgpiopin"])
 	self.invertgpiopins = self._settings.get(["invertgpiopins"])
 
 	#Print logging output
         self._logger.info("OctoPrint PowerManager is starting up.")
         self._logger.info("Power GPIO Pin:   [%s]"%self.powergpiopin)
-	self._logger.info("Fan GPIO Pin:     [%s]"%self.fangpiopin)
         self._logger.info("Printer GPIO Pin: [%s]"%self.printergpiopin)
+        self._logger.info("Light GPIO Pin:  [%s]"%self.lightgpiopin)
 	self._logger.info("Invert GPIO pins: [%s]"%self.invertgpiopins)
 
 	#Disable GPIO warnings and set pin labeling mode.
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 
-        #Set I/O modes if GPIOs are defined.
+        #Sets ON values
+        #If inverted, "ON" = FALSE for active-on-low boards.
+
+        if self.invertgpiopins:
+            ON = False
+            OFF = True
+            self._logger.info("Using Inverted tables for GPIO")
+        else:
+            ON = True
+            OFF = False
+            self._logger.info("Using Normal tables for GPIO")
+
+
+
+        #Set mode OUTPUT for GPIO pins
+        GPIO.setup(int(self.powergpiopin), GPIO.OUT)
+        GPIO.setup(int(self.lightgpiopin), GPIO.OUT)
+        GPIO.setup(int(self.printergpiopin), GPIO.OUT)
+
+
+        #Sets status flags and initial start
+	GPIO.output(int(self.powergpiopin), On) #Turn on power supply.
+	GPIO.output(int(self.printergpiopin), On) #Turn on printer.
+        GPIO.output(int(self.lightgpiopin), On #Turn on lights.
+
 
     def on_after_startup(self):
         self._logger.info("Hello World! more %s)" % self._settings.get(["url"]))
 
+
+    #Tidy up after shutting down Octoprint.
+    def on_shutdown(self):
+        GPIO.output(self.printergpiopin, OFF) #Turn off printer
+        GPIO.output(self.lightgpiopin, OFF)   #Lights off
+	GPIO.output(self.powergpiopin, OFF)   #Turn off power supply.
+	GPIO.cleanup()
+
+
     def get_settings_defaults(self):
         return dict(
               powergpiopin="-1",
-              fangpiopin="-1",
+              lightgpiopin="-1",
               printergpiopin="-1",
               invertgpiopins="-1"
         )
@@ -47,7 +80,7 @@ class PowerManagerPlugin(octoprint.plugin.StartupPlugin,
     def get_template_vars(self):
         return dict(
               powergpiopin=self._settings.get(["powergpiopin"]),
-              fangpiopin=self._settings.get(["fangpiopin"]),
+              lightgpiopin=self._settings.get(["lightgpiopin"]),
               printergpiopin=self._settings.get(["printergpiopin"])
         )
 
