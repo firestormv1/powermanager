76cdb0b92b50b8b664f7a1872fd133a3179f3728
firestormv1 <firestormv1@users.noreply.github.com>
2016-09-30 11:08:39 -0500

    Final version increment for release
    Release version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_powermanager"
 plugin_name = "OctoPrint-PowerManager"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.0.BETA"
+plugin_version = "1.0.0.RELEASE"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
